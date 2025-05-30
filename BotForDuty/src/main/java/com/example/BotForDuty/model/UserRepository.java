979bd3d354fda2a2e9111fe6d7a9ed3e794f6c3d
Aman <115641879+mynamestethom@users.noreply.github.com>
2025-05-30 23:04:47 +0500

    Delete BotForDuty directory

--- BotForDuty/src/main/java/com/example/BotForDuty/model/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.BotForDuty.model;
-
-import com.example.BotForDuty.pojo.Person;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<Person, Long> {}
